b425782f2238042868881213acb499c2339bfddb
文彥 <xx48833@gmail.com>
2024-12-10 00:41:05 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+int NNN(int n, int counting){
+    if (counting==1){
+        printf("\n %d",n);
+        return n;
+    }
+    else{
+        int temp=0;
+        temp=n*pow(10,counting-1);
+        printf("\ntemp %d",temp);
+        return (temp+NNN(n,counting-1));
+    }
+}
+
+int main(){
+    int n, ans, counting;
+    scanf("%d",&n);
+    counting = n;
+    ans=NNN(n,counting);
+    printf("\n%d",ans);
+}
